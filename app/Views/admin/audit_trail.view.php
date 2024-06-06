9f4596915e8eb1982ed78c508ec34d0e09f2838c
jhypsss <jhonpatrick.torres@tup.edu.ph>
2024-06-06 23:39:17 +0800

    Added Search Bar

--- app/Views/admin/audit_trail.view.php
+++ app/Views/admin/audit_trail.view.php
@@ -3,10 +3,42 @@
         background-color: #C23540; 
         color: #fff;
     }
+    .task-roll-up {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        margin-top: 50px;
+    }
+    .no-items-message {
+        font-size: 20px;
+        color: #666;
+        margin-bottom: 20px;
+        display: flex;
+        align-items: center;
+    }
+    .icon {
+        font-size: 4em;
+    }
+    .btn-primary {
+        border: none;
+        color: white;
+        width: 100px;
+    }
+    .btn-success {
+        border: none;
+        color: white;
+        padding: 10px;
+    }
+    .btn-danger {
+        border: none;
+        color: white;
+        padding: 10px;
+    }
 </style>
-
+<?php if (!empty($audit_trails)):?>
 <div class="table-responsive" style="height: 650px;overflow-y: scroll;">
     <!-- Table section -->
+    <input type="text" class="form-control" id="searchInput" placeholder="Search..." style="width: 50%; float: right;"><br><br>
     <table class="table table-striped table-hover">
     <thead class="table-red" style="position: sticky;top: 0; background-color: #C23540;">
         <tr>
@@ -18,7 +50,7 @@
         </tr>
         </thead>
         <tbody>
-        <?php if (!empty($audit_trails)):?>
+        
             <?php foreach ($audit_trails as $audit_trail):?>
                 <tr>
                     <td><?=date("M j, Y, h:ia",strtotime($audit_trail['date']))?></td>
@@ -45,7 +77,38 @@
                     
                 </tr>
             <?php endforeach;?>
-        <?php endif;?>
         </tbody>
     </table>
+    <?php else:?>
+        <div class="task-roll-up">
+            <i class="icon fas fa-file-alt"></i>
+            <p class="no-items-message"> There's no record yet.</p>
+        </div>
+    <?php endif;?>
 </div>
+
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var searchInput = document.getElementById('searchInput');
+        var tableRows = document.querySelectorAll('.table tbody tr');
+
+        // Search functionality
+        searchInput.addEventListener('keyup', function(event) {
+            var query = event.target.value.toLowerCase();
+
+            tableRows.forEach(function(row) {
+                var cells = row.getElementsByTagName('td');
+                var found = false;
+
+                Array.from(cells).forEach(function(cell) {
+                    var cellText = cell.textContent.toLowerCase();
+                    if (cellText.indexOf(query) > -1) {
+                        found = true;
+                    }
+                });
+
+                row.style.display = found ? '' : 'none';
+            });
+        });
+    });
+</script>
